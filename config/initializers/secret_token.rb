e9033d11986014d2eb6b683d6feb78663634ef51
Uko <uko@unikernel.net>
2012-04-18 13:02:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConceptGraph::Application.config.secret_token = 'f6193bea7bbfee06dae4ef06a1fde64ff02c0a315e7e06c550fe1a214dd541544cb4679ae3ef42415ada1cb0a216dc809eacd15603fee62e21b23fab0ff23418'
