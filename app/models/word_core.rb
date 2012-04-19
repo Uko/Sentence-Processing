03bd0910b0f2cba8898e15257812bb10835b2755
anygizer <mcangel@unikernel.net>
2012-04-19 07:34:57 +0300

    Added initial db pupolation migration. Added adverb part type.

--- app/models/word_core.rb
+++ app/models/word_core.rb
@@ -1,9 +1,9 @@
 class WordCore < ActiveRecord::Base
-  attr_accessible :infinitive, :part
+	attr_accessible :infinitive, :part
 	has_many :words, :dependent => :destroy
 	def self.all_parts
-    %w(noun verb adjective)
-  end
+		%w(noun verb adjective adverb)
+	end
 	validates_presence_of :infinitive, :part
 	validates_uniqueness_of :infinitive
 	validates_inclusion_of :part, :in => self.all_parts, :message => "part of language: %s is unknown"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120418103840) do
+ActiveRecord::Schema.define(:version => 20120419022812) do
 
   create_table "word_cores", :force => true do |t|
     t.string   "infinitive"
